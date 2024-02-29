eb6183268850ca281b444d4fbb152c6b4d3f4390
Michael Pacheco <michaelpacheco.br@gmail.com>
2024-02-29 00:00:27 -0300

    task: front configs

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,19 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+
+import viteLogo from "/vite.svg";
+
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          href="https://react.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +47,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import * as path from "path";
+
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    build: {
+      sourcemap: false,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
+});
