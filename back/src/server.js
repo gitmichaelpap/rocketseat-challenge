6cee3d0d45c82bcec932985ea169c1feda6138b8
Michael Pacheco <michaelpacheco.br@gmail.com>
2024-07-23 22:27:24 -0300

    env e deploy back

--- back/src/server.js
+++ back/src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors");
-// require("dotenv/config");
+require("dotenv/config");
 
 const uploadConfig = require("./configs/upload");
 const cors = require("cors");
@@ -36,5 +36,5 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
